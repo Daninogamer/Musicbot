d0e9924fadb91bbd38456475494111466954bff6
Daninogamer <88000948+Daninogamer@users.noreply.github.com>
2021-10-01 21:50:17 +0200

    fix

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -7,11 +7,11 @@ module.exports = {
     async execute(client, message, args) {
         const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+        if (!queue || !queue.playing) return message.channel.send(`Nessuna canzone in riproduzione ${message.author}... Riprova! ❌`);
 
         const actualFilter = queue.getFiltersEnabled()[0];
 
-        if (!args[0]) return message.channel.send(`Please specify a valid filter to enable or disable ${message.author}... try again ? ❌\n${actualFilter ? `Filter currently active ${actualFilter} (${client.config.app.px}filter ${actualFilter} to disable it).\n` : ''}`);
+        if (!args[0]) return message.channel.send(`Si prega di specificare un filtro valido per abilitare o disabilitare ${message.author}... Riprova! ❌\n${actualFilter ? `Filtro corrente ${actualFilter} (${client.config.app.px}filtro ${actualFilter} per disabilitarlo).\n` : ''}`);
 
         const filters = [];
 
@@ -20,7 +20,7 @@ module.exports = {
 
         const filter = filters.find((x) => x.toLowerCase() === args[0].toLowerCase());
 
-        if (!filter) return message.channel.send(`This filter doesn't exist ${message.author}... try again ? ❌\n${actualFilter ? `Filter currently active ${actualFilter}.\n` : ''}List of available filters ${filters.map(x => `**${x}**`).join(', ')}.`);
+        if (!filter) return message.channel.send(`Questo filtro non esiste! ${message.author}... Riprova! ❌\n${actualFilter ? `Filtro correnre ${actualFilter}.\n` : ''}Lista dei vari filtri disponibili ${filters.map(x => `**${x}**`).join(', ')}.`);
 
         const filtersUpdated = {};
 
@@ -28,6 +28,6 @@ module.exports = {
 
         await queue.setFilters(filtersUpdated);
 
-        message.channel.send(`The filter ${filter} is now **${queue.getFiltersEnabled().includes(filter) ? 'enabled' : 'disabled'}** ✅\n*Reminder the longer the music is, the longer this will take.*`);
+        message.channel.send(`Il filtro ${filter} è ora **${queue.getFiltersEnabled().includes(filter) ? 'enabled' : 'disabled'}** ✅\n*Ricorda che più lunga è la canzone, più tempo ci vorrà.*`);
     },
 };
